2bea1b1155583a87c5d301118e025fa2cb1693ef
carissagregory <carissaegregory@gmail.com>
2025-04-15 11:37:30 -0500

    Updating login page

--- page-login.php
+++ page-login.php
@@ -11,8 +11,13 @@
  ?>
  <div class="row">
 	 <div class="col-md-8 order-md-2 col-sm-12">
-		 <div id="post-<?php the_ID(); ?>" <?php post_class( 'content' ); ?>>
+		<div id="post-<?php the_ID(); ?>" <?php post_class( 'content' ); ?>>
+        <form>
+
+        </form>
 			 <h1 class="entry-title"><?php the_title(); ?></h1>
+             <h2><?php the_field("contact_email"); ?></h2>
+             <?php $value = get_field( "password", "option"); ?>
  </div><!-- /.row -->
  <?php
  get_footer();
