7b7198b62bc9b79dffa29cafd545a75f62892c9e
carissagregory <carissaegregory@gmail.com>
2025-04-15 11:20:34 -0500

    updated footer styling

---
+++ page-login.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Template Name: Log In Page
+ * Description: Page template with User Log In Fields
+ *
+ */
+
+ get_header();
+
+ the_post();
+ ?>
+ <div class="row">
+	 <div class="col-md-8 order-md-2 col-sm-12">
+		 <div id="post-<?php the_ID(); ?>" <?php post_class( 'content' ); ?>>
+			 <h1 class="entry-title"><?php the_title(); ?></h1>
+ </div><!-- /.row -->
+ <?php
+ get_footer();
