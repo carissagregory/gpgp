c3f7131f2ef560243d20dc0d7441ec65ff1d4b02
carissagregory <carissaegregory@gmail.com>
2025-04-17 10:23:21 -0500

    updating front-page and adding img

--- front-page.php
+++ front-page.php
@@ -12,12 +12,14 @@ the_post();
 <div class="row">
 	<div class="col-md-8 order-md-2 col-sm-12">
 		<div id="post-<?php the_ID(); ?>" <?php post_class( 'content' ); ?>>
-			<h1 class="entry-title"><?php the_title(); ?></h1>
-			<div class="scroll-container">
-				<img src="/assets/images/gamingImages/greenDice.jpg" alt="green dice" width="600" height="400">
-				<img src="/assets/images/gamingImages/greenDice.jpg" alt="green dice" width="600" height="400">
-				<img src="/assets/images/gamingImages/greenDice.jpg" alt="green dice" width="600" height="400">
-				<img src="/assets/images/gamingImages/greenDice.jpg" alt="green dice" width="600" height="400">
+			<h1 class="entry-title"><?php the_title(); ?></h1>	
+			<div class="row">
+				<div class="col-md-6">
+					<img src="<?php echo get_template_directory_uri(); ?>/assets/images/gamingImages/figurines.png" alt="figurines" id="mainImg" class="img-fluid">
+				</div>
+				<div class="col-md-6">
+					<img src="<?php echo get_template_directory_uri(); ?>/assets/images/gamingImages/aboutGPGP.png" alt="about gpgp" id="aboutImg" class="img-fluid">
+				</div>
 			</div>
 </div><!-- /.row -->
 <?php
