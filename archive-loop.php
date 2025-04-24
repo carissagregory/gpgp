1ec4fe579584a600cb66724bef08c7afd0026d48
2bLen <lentechwise@gmail.com>
2025-04-24 08:28:50 -0500

    Update archive-loop.php

--- archive-loop.php
+++ archive-loop.php
@@ -1,12 +1,14 @@
 <?php
 /**
- * Template for displaying archive/game posts loop
+ * The template for displaying all single posts
  */
 
-if ( have_posts() ) :
-    echo '<div class="gameGrid">';
-    while ( have_posts() ) : the_post();
+get_header();
 
+while ( have_posts() ) :
+    the_post();
+
+    if ( in_category('games') ) {
         $color_slug = trim(get_field('game_color'));
         $valid_colors = ['darkBlue', 'darkRed', 'gold', 'lightBlue', 'cream', 'black', 'white'];
 
@@ -16,26 +18,28 @@ if ( have_posts() ) :
             $background_class = 'gameCard gameCardDefault';
         }
 
-        echo '<div class="gameCard ' . esc_attr($background_class) . '">';
-            // Featured image
-            if ( has_post_thumbnail() ) {
-                echo '<div class="gameCardImage">';
-                the_post_thumbnail('medium');
-                echo '</div>';
-            }
+        echo '<div class="' . esc_attr($background_class) . '">';
 
-            echo '<h2 class="gameCardTitle">' . get_the_title() . '</h2>';
-            echo '<div class="gameCardPlayers"><span>Players:</span> ' . get_field('game_players') . '</div>';
-            echo '<div class="gameCardInfo"><span>Info:</span> ' . get_field('game_info') . '</div>';
-            echo '<div class="gameCardGoal"><span>Goal:</span> ' . get_field('game_goal') . '</div>';
-            echo '<div class="gameCardRules"><span>Rules:</span> ' . get_field('game_rules') . '</div>';
-            echo '<div class="gameCardOther"><span>Other:</span> ' . get_field('game_other') . '</div>';
-            echo '<div class="gameCardDetails">' . get_field('game_description') . '</div>';
+        if ( has_post_thumbnail() ) {
+            echo '<div class="gameCardImage">';
+            the_post_thumbnail('medium');
+            echo '</div>';
+        }
+
+        echo '<h2 class="gameCardTitle">' . get_the_title() . '</h2>';
+        echo '<div class="gameCardPlayers"><span>Players:</span> ' . get_field('game_players') . '</div>';
+        echo '<div class="gameCardInfo"><span>Info:</span> ' . get_field('game_info') . '</div>';
+        echo '<div class="gameCardGoal"><span>Goal:</span> ' . get_field('game_goal') . '</div>';
+        echo '<div class="gameCardRules"><span>Rules:</span> ' . get_field('game_rules') . '</div>';
+        echo '<div class="gameCardOther"><span>Other:</span> ' . get_field('game_other') . '</div>';
+        echo '<div class="gameCardDetails">' . get_field('game_description') . '</div>';
+
         echo '</div>';
+    } else {
+        get_template_part( 'content', 'single' );
+    }
 
-    endwhile;
-    echo '</div>';
-else :
-    echo '<p>No posts found.</p>';
-endif;
+endwhile;
+
+get_footer();
 ?>
