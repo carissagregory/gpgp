b139ea332e8750da72b10e202f78682eae827157
2bLen <lentechwise@gmail.com>
2025-04-24 07:05:19 -0500

    game page
    getting there

--- archive-loop.php
+++ archive-loop.php
@@ -11,8 +11,19 @@ if ( have_posts() ) :
         $background_class = $color_slug ? 'gameCard' . ucfirst($color_slug) : 'gameCardDefault';
 
         echo '<div class="gameCard ' . esc_attr($background_class) . '">';
+            // Featured image
+            if ( has_post_thumbnail() ) {
+                echo '<div class="gameCardImage">';
+                the_post_thumbnail('medium');
+                echo '</div>';
+            }
+
             echo '<h2 class="gameCardTitle">' . get_the_title() . '</h2>';
             echo '<div class="gameCardPlayers"><span>Players:</span> ' . get_field('game_players') . '</div>';
+            echo '<div class="gameCardInfo"><span>Info:</span> ' . get_field('game_info') . '</div>';
+            echo '<div class="gameCardGoal"><span>Goal:</span> ' . get_field('game_goal') . '</div>';
+            echo '<div class="gameCardRules"><span>Rules:</span> ' . get_field('game_rules') . '</div>';
+            echo '<div class="gameCardOther"><span>Other:</span> ' . get_field('game_other') . '</div>';
             echo '<div class="gameCardDetails">' . get_field('game_description') . '</div>';
         echo '</div>';
 
